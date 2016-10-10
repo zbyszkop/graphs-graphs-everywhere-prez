ad7c22e49c1369536bf06efc15db0bf513ba4b8f
Zbyszko Papierski <zpapierski@atlassian.com>
2016-10-10 12:33:19 +0200

    WIP

--- vis/js/main.js
+++ vis/js/main.js
@@ -65,12 +65,12 @@ function requestNewGraph() {
                 cy.layout({
                     name: 'breadthfirst'
                 });
-
+                cy.fit();
             })
     );
 }
 
-function hashCode(str) { // java String#hashCode
+function hashCode(str) {
     var hash = 0;
     for (var i = 0; i < str.length; i++) {
         hash = str.charCodeAt(i) + ((hash << 5) - hash);
